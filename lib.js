d7fd2e0a7bf3dc2f184a876d63a7e79422395534
fruitmstr <136752403+fruitmstr@users.noreply.github.com>
2023-07-31 20:56:08 -0700

    Update lib.js

--- lib.js
+++ lib.js
@@ -18373,14 +18373,13 @@ function gotDevices2(deviceInfos) {
 
 				label.innerHTML = " " + (deviceInfo.label || ("microphone " + ((audioInputSelect.length || 0) + 1)));
 				
-				//[V3] Next attempt at killing second wave SquireInput
-				// if (!option.label.includes("SquireInput")){
-				// }
-				console.log(deviceInfo.label)
-				
-				listele.appendChild(option);
-				listele.appendChild(label);
-				audioInputSelect.appendChild(listele);
+				//[V3] Next attempt at killing second wave SquireInput with the if statement enclosure
+				// console.log(deviceInfo.label)
+				if (!deviceInfo.label.includes("SquireInput")){
+					listele.appendChild(option);
+					listele.appendChild(label);
+					audioInputSelect.appendChild(listele);
+				}
 
 				option.onchange = function(event) { // make sure to clear 'no audio option' if anything else is selected
 					log("change 4768");
